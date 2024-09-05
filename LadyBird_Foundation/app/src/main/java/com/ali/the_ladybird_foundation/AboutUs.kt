7dc809ef52a9fd0d270c5f0780d36f5eeb701adb
Alison Rees <st10038986@vcconnect.edu.za>
2024-09-05 18:27:06 +0200

    Added Home page, Backend code for splash screen, adjusted a few images

---
+++ LadyBird_Foundation/app/src/main/java/com/ali/the_ladybird_foundation/AboutUs.kt
@@ -0,0 +1,11 @@
+package com.ali.the_ladybird_foundation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutUs : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+    }
+}
