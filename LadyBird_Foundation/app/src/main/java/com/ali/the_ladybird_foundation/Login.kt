966e89db43df624c8414691ff26c75f801d5cb01
Alison Rees <st10038986@vcconnect.edu.za>
2024-09-05 13:34:14 +0200

    Splash Screen, Login and registration screen update

---
+++ LadyBird_Foundation/app/src/main/java/com/ali/the_ladybird_foundation/Login.kt
@@ -0,0 +1,11 @@
+package com.ali.the_ladybird_foundation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ LadyBird_Foundation/app/src/main/java/com/ali/the_ladybird_foundation/Regsiter.kt
@@ -0,0 +1,11 @@
+package com.ali.the_ladybird_foundation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Regsiter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regsiter)
+    }
+}
