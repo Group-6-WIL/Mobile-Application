e1b4945cc8af13fedb395d82ea00330301b0c0de
Alison Rees <st10038986@vcconnect.edu.za>
2024-09-07 13:20:47 +0200

    ImageViews added to all Pages

---
+++ LadyBird_Foundation/app/src/main/java/com/ali/the_ladybird_foundation/Location.kt
@@ -0,0 +1,11 @@
+package com.ali.the_ladybird_foundation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Location : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
+    }
+}
