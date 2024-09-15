bc59aab30dafcce9b3b5c5ec33875663f819a790
Alison Rees <st10038986@vcconnect.edu.za>
2024-09-15 17:09:42 +0200

    Commit design changes

---
+++ LadyBird_Foundation/app/src/main/java/com/ali/the_ladybird_foundation/admin_events.kt
@@ -0,0 +1,11 @@
+package com.ali.the_ladybird_foundation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class admin_events : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_events)
+    }
+}
