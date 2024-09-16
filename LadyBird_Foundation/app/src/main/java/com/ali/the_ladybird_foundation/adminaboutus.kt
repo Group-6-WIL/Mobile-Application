a01e86c8b44af5ab19ec420b7e412ca5364b164c
Alison Rees <st10038986@vcconnect.edu.za>
2024-09-16 08:08:06 +0200

    updated design

---
+++ LadyBird_Foundation/app/src/main/java/com/ali/the_ladybird_foundation/adminaboutus.kt
@@ -0,0 +1,11 @@
+package com.ali.the_ladybird_foundation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class adminaboutus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adminaboutus)
+    }
+}
