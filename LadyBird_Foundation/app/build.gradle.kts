60cc5fb9f9c39754e7557d960079f9d9324d61ce
Alison Rees <st10038986@vcconnect.edu.za>
2024-10-25 13:58:22 +0200

    new aab

--- LadyBird_Foundation/app/build.gradle.kts
+++ LadyBird_Foundation/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ali.the_ladybird_foundation"
         minSdk = 27
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
